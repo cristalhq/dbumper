fe9b29509777579f163555ab5af724442afc947c
Oleg Kovalov <oleg@hey.com>
2021-08-07 20:31:02 +0200

    Simpler command new
    Signed-off-by: Oleg Kovalov <oleg@hey.com>

--- cmd_new.go
+++ cmd_new.go
@@ -5,9 +5,8 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"regexp"
-	"strconv"
-	"strings"
+
+	"github.com/cristalhq/dbumper/dbump"
 )
 
 type configNew struct {
@@ -15,13 +14,11 @@ type configNew struct {
 	Name string `default:"NONAME"`
 }
 
-var migrationRE = regexp.MustCompile(`^(\d+)_.+\.sql$`)
-
-var newMigrationText = `--- [Type name of the migration here]
-
---- apply above / rollback below ---
-
-`
+var newMigrationText = `` +
+	`--- [Type name of the migration here]` +
+	"\n\n" +
+	dbump.MigrationDelimiter +
+	"\n"
 
 func newMigrationCmd(_ context.Context) error {
 	var cfg configNew
@@ -30,7 +27,7 @@ func newMigrationCmd(_ context.Context) error {
 	}
 	path := cfg.Path
 
-	migrations, err := loadMigrations(path)
+	migrations, err := dbump.NewDiskLoader(path).Load()
 	if err != nil {
 		return fmt.Errorf("cannot load migrations: %w", err)
 	}
@@ -58,41 +55,4 @@ func createNewMigration(path, name string) error {
 
 	_, err := newMig.WriteString(newMigrationText)
 	return err
-}
-
-// TODO: reuse dbump.Loader
-func loadMigrations(path string) ([]string, error) {
-	path = strings.TrimRight(path, string(filepath.Separator))
-
-	files, err := os.ReadDir(path)
-	if err != nil {
-		return nil, err
-	}
-
-	paths := make([]string, 0, len(files))
-	for _, fi := range files {
-		if fi.IsDir() {
-			continue
-		}
-
-		matches := migrationRE.FindStringSubmatch(fi.Name())
-		if len(matches) != 2 {
-			continue
-		}
-
-		n, err := strconv.ParseInt(matches[1], 10, 32)
-		if err != nil {
-			return nil, err
-		}
-
-		switch id := int(n); {
-		case id < len(paths)+1:
-			return nil, fmt.Errorf("duplicate migration %d", id)
-		case len(paths)+1 < id:
-			return nil, fmt.Errorf("missing migration %d", len(paths)+1)
-		default:
-			paths = append(paths, filepath.Join(path, fi.Name()))
-		}
-	}
-	return paths, nil
 }
