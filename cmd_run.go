7cf4ef2bc30c7e2c58d42f4d04f98b8f93d7a9c6
Oleg Kovalov <oleg@hey.com>
2021-08-07 17:15:37 +0200

    Use new dbump submodule
    Signed-off-by: Oleg Kovalov <oleg@hey.com>

--- cmd_run.go
+++ cmd_run.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 
 	"github.com/cristalhq/dbumper/dbump"
-	"github.com/cristalhq/dbumper/dbumpmysql"
-	"github.com/cristalhq/dbumper/dbumppg"
 )
 
 type ConfigRun struct {
@@ -21,12 +19,12 @@ func runCmd(ctx context.Context) error {
 	var migrator dbump.Migrator
 	switch cfg.DB {
 	case "postgres":
-		migrator = dbumppg.NewMigrator(nil)
+		migrator = dbump.NewMigratorPostgres(nil)
 	case "mysql":
-		migrator = dbumpmysql.NewMigrator(nil)
+		migrator = dbump.NewMigratorMySQL(nil)
 	default:
 		return fmt.Errorf("unsupported DB: %s", cfg.DB)
 	}
 
-	return dbump.Run(ctx, migrator, dbump.NewRealFS(cfg.Path))
+	return dbump.Run(ctx, migrator, nil)
 }
--- go.mod
+++ go.mod
@@ -4,7 +4,5 @@ go 1.16
 
 require (
 	github.com/cristalhq/aconfig v0.16.2
-	github.com/cristalhq/dbumper/dbump v0.0.0-20210807080839-ddbf705f1719
-	github.com/cristalhq/dbumper/dbumpmysql v0.0.0-20210807070854-817b81988f9e
-	github.com/cristalhq/dbumper/dbumppg v0.0.0-20210807070854-817b81988f9e
+	github.com/cristalhq/dbumper/dbump v0.0.0-20210807123540-7af4d04a6592
 )
