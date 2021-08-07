a4e0fcde6e406f798e96d0c4e22ec79b4aa1d14d
Oleg Kovalov <oleg@hey.com>
2021-08-07 20:26:12 +0200

    Enable run for DBs
    Signed-off-by: Oleg Kovalov <oleg@hey.com>

--- cmd_run.go
+++ cmd_run.go
@@ -2,29 +2,54 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 
 	"github.com/cristalhq/dbumper/dbump"
+
+	_ "github.com/ClickHouse/clickhouse-go" // import ClickHouse
+	_ "github.com/go-sql-driver/mysql"      // import MySQL
+	_ "github.com/jackc/pgx/v4/stdlib"      // import Postgres (pgx-stdlib)
 )
 
 type configRun struct {
 	Path string `default:"./migrations"`
 	DB   string `default:"UNKNOWN"`
+	DSN  string `default:"UNKNOWN"`
 }
 
 func runCmd(ctx context.Context) error {
 	var cfg configRun
+	if err := loadConfig(&cfg); err != nil {
+		return err
+	}
 
-	// TODO: init *sql.DB
 	var migrator dbump.Migrator
 	switch cfg.DB {
-	case "postgres":
-		migrator = dbump.NewMigratorPostgres(nil)
+	case "clickhouse":
+		db, err := sql.Open("clickhouse", cfg.DSN)
+		if err != nil {
+			return err
+		}
+		migrator = dbump.NewMigratorClickHouse(db)
+
 	case "mysql":
-		migrator = dbump.NewMigratorMySQL(nil)
+		db, err := sql.Open("mysql", cfg.DSN)
+		if err != nil {
+			return err
+		}
+		migrator = dbump.NewMigratorMySQL(db)
+
+	case "postgres":
+		db, err := sql.Open("pgx", cfg.DSN)
+		if err != nil {
+			return err
+		}
+		migrator = dbump.NewMigratorPostgres(db)
+
 	default:
 		return fmt.Errorf("unsupported DB: %s", cfg.DB)
 	}
 
-	return dbump.Run(ctx, migrator, nil)
+	return dbump.Run(ctx, migrator, dbump.NewDiskLoader(cfg.Path))
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module github.com/cristalhq/dbumper
 go 1.16
 
 require (
+	github.com/ClickHouse/clickhouse-go v1.4.5
 	github.com/cristalhq/aconfig v0.16.2
 	github.com/cristalhq/dbumper/dbump v0.0.3
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/jackc/pgx/v4 v4.13.0
 )
