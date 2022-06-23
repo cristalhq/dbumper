7bd05f648256d12aae3f47f85fcaa6b97114db9a
Oleg Kovalov <oleg@hey.com>
2022-06-23 19:01:09 +0200

    Cleanups in init

--- cmd_init.go
+++ cmd_init.go
@@ -7,9 +7,7 @@ import (
 )
 
 type configInit struct {
-	Path         string `default:"./migrations"`
-	VersionTable string `default:"_dbumper_version"`
-	RemoveOld    bool   `default:"false"`
+	Path string `default:"./migrations"`
 }
 
 func initFolderCmd(_ context.Context, _ []string) error {
@@ -18,12 +16,7 @@ func initFolderCmd(_ context.Context, _ []string) error {
 		return err
 	}
 
-	if cfg.RemoveOld {
-		if err := os.RemoveAll(cfg.Path); err != nil {
-			return fmt.Errorf("cannot remove directory: %w", err)
-		}
-	}
-	if err := os.Mkdir(cfg.Path, os.ModePerm); err != nil {
+	if err := os.MkdirAll(cfg.Path, os.ModePerm); err != nil {
 		return fmt.Errorf("cannot create directory: %w", err)
 	}
 	return nil
